866f692b029eb1ac98a9fb5c389cc91752194b3d
Grzegorz <s12583@pjwstk.edu.pl>
2020-10-08 23:07:21 +0200

    Prepare style for a create account component
    Took 2 hours 3 minutes

--- src/app/modules/accounts/create-account/create-account.component.ts
+++ src/app/modules/accounts/create-account/create-account.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {CdkStepper} from '@angular/cdk/stepper';
-import {CustomStepperComponent} from '../custom-stepper/custom-stepper.component';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+interface GenderType {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-create-account',
@@ -10,12 +14,45 @@ import {CustomStepperComponent} from '../custom-stepper/custom-stepper.component
 export class CreateAccountComponent implements OnInit {
 
   selectedTab: string;
+  form: FormGroup;
+  value: any;
 
-  constructor() {
+  genderTypes: GenderType[] = [
+    {value: 'MALE', viewValue: 'Chłopiec'},
+    {value: 'FEMALE', viewValue: 'Dziewczynka'}
+  ];
+  readOnly = false;
+
+
+  constructor(private fb: FormBuilder) {
     this.selectedTab = 'child';
   }
 
   ngOnInit(): void {
+
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      surname: ['', []],
+      gender: ['', []],
+      pesel: ['', []],
+      dateOfBirth: ['', []],
+      postalCode: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      streetNumber: ['', [Validators.required]],
+      status: ['', []],
+      startDate: ['', [Validators.required]],
+      endDate: ['', [Validators.required]],
+      group: ['', [Validators.required]],
+      commentsAdministration: ['', [Validators.required]],
+      commentsMeals: ['', [Validators.required]],
+      commentsOthers: ['', [Validators.required]],
+    });
+
+    this.form.valueChanges.subscribe(val => {
+      this.value = val;
+      console.log(val);
+    });
+
   }
 
   onSelectTab(type: string) {
